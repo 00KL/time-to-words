d9d575bb51c9af1148afadf17d3aae9d92a155db
00kl <lucasmoraesplay@gmail.com>
2025-12-17 22:04:48 -0300

    Original code

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
   // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
-  }
+  if (time === "0:00") {
+    return "midnight";
+  } else if (time === "12:00") {
+    return "midday";
+  } else {
+    const [hour, minutes] = time.split(":");
 
-  return 'half past eight';
+    const timeRecord = {
+      1: "one",
+      2: "two",
+      3: "three",
+      4: "four",
+      5: "five",
+      6: "six",
+      7: "seven",
+      8: "eight",
+      9: "nine",
+      10: "ten",
+      11: "eleven",
+      12: "twelve",
+      13: "thirteen",
+      14: "fourteen",
+      15: "fifteen",
+      16: "sixteen",
+      17: "seventeen",
+      18: "eighteen",
+      19: "nineteen",
+      20: "twenty",
+      21: "twenty one",
+      22: "twenty two",
+      23: "twenty three",
+      24: "twenty four",
+      25: "twenty five",
+      26: "twenty six",
+      27: "twenty seven",
+      28: "twenty eight",
+      29: "twenty nine",
+      30: "half",
+    };
+
+    const hourString = timeRecord[hour];
+
+    if (minutes == "00") {
+      return `${hourString} o'clock`;
+    } else {
+      if (Number(minutes) <= 30) {
+        if (minutes == "15") {
+          return `quarter past ${timeRecord[hour]}`;
+        }
+
+        return `${timeRecord[Number(minutes)]} past ${timeRecord[hour]}`;
+      } else {
+        const remaingTime = 60 - Number(minutes);
+
+        if (remaingTime == 15) {
+          return `quarter to ${timeRecord[Number(hour) + 1]}`;
+        }
+
+        return `${timeRecord[remaingTime]} to ${timeRecord[Number(hour) + 1]}`;
+      }
+    }
+  }
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,18 +1,48 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require("./index");
 
-describe('Time to words', () => {
-  it('Handles midnight', () => {
-    const timeInWords = convertTimeToWords('0:00');
-    expect(timeInWords).toBe('midnight');
+describe("Time to words", () => {
+  it("Handles midnight", () => {
+    const timeInWords = convertTimeToWords("0:00");
+    expect(timeInWords).toBe("midnight");
   });
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
+  it("Handles midday", () => {
+    const timeInWords = convertTimeToWords("12:00");
+    expect(timeInWords).toBe("midday");
   });
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
+  it("2:00", () => {
+    const timeInWords = convertTimeToWords("2:00");
+    expect(timeInWords).toBe("two o'clock");
+  });
+
+  it("2:03", () => {
+    const timeInWords = convertTimeToWords("2:03");
+    expect(timeInWords).toBe("three past two");
+  });
+
+  it("2:33", () => {
+    const timeInWords = convertTimeToWords("2:33");
+    expect(timeInWords).toBe("twenty seven to three");
+  });
+
+  it("2:45", () => {
+    const timeInWords = convertTimeToWords("2:45");
+    expect(timeInWords).toBe("quarter to three");
+  });
+
+  it("Handles 30 - 8:30", () => {
+    const timeInWords = convertTimeToWords("8:30");
+    expect(timeInWords).toBe("half past eight");
+  });
+
+  it("8:15", () => {
+    const timeInWords = convertTimeToWords("8:15");
+    expect(timeInWords).toBe("quarter past eight");
+  });
+
+  it("Handles times after 30 mins - 2:45", () => {
+    const timeInWords = convertTimeToWords("2:45");
+    expect(timeInWords).toBe("quarter to three");
   });
 });
